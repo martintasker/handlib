af2b29cb7c90219b54439d392769721c866d2cb9
Martin Tasker <martin.tasker@databatix.com>
2016-10-19 13:32:31 +0100

    release v0.4.0

--- README.md
+++ README.md
@@ -42,3 +42,19 @@ bower install --save git@192.168.0.136:/opt/git/handlib#v1.2.3
 
 or, to update, change the version specified in `bower.json` to specify `v1.2.3`, or `^1.2.3`, or such,
 and `bower update handlib`.
+
+## releases
+
+### v0.4.0
+
+Compared with v0.3.0,
+
+* the internal dependency on `GlyphFeatures` has been eliminated and GlyphFeatures deleted from the library
+* the API to `FXGlyph` has been made entirely read-only, comprising getters either of glyph set at construct time or lazily-extracted features
+* `bbox` is no longer in `Glyph`
+* `FXGlyph.subStrokes` is based on unit-scaled strokes, now also available as `FXGlyph.unitScaledStrokes` -- previously they were scaled the same as
+  the glyph's `strokes`
+* similarly, `FXGlyph.size` and `FXGlyph.sizes` are unit-scaled
+* besides using a `GlyphBuilder`, you can construct a glyph directly using `new Glyph(spec)`: the `spec` parameter allows you to specify `id`, `strokes` and `device`
+* the strokes given to the `Glyph` constructor may be specified either in sequential form eg `[[0,0,100,100]]` or struct form eg `[{x:0,y:0],{x:100,y:100}]`
+* all this is well tested
--- handlib.js
+++ handlib.js
@@ -5,46 +5,241 @@ module.exports.Glyph = require('./lib/glyph');
 module.exports.GlyphBuilder = require('./lib/glyph-builder');
 module.exports.FXGlyph = require('./lib/fx-glyph');
 
-},{"./lib/fx-glyph":2,"./lib/glyph":5,"./lib/glyph-builder":3}],2:[function(require,module,exports){
+},{"./lib/fx-glyph":2,"./lib/glyph":4,"./lib/glyph-builder":3}],2:[function(require,module,exports){
 'use strict';
 
 var Glyph = require('./glyph');
-var GlyphFeatures = require('./glyph-features');
 
 var FXGlyph = function(glyph) {
-
-  // public API and instance variables
-  var self = this;
-  self.glyph = glyph;
+  this._glyph = glyph;
 };
 
+// getters
 FXGlyph.prototype = {
+  get glyph() {
+    return this._glyph;
+  },
   get id() {
-    return this.glyph.id;
+    return this._glyph.id;
   },
   get bbox() {
-    return this.glyph.bbox;
+    if (!this._bbox) {
+      this._bbox = this._getBBox(this._glyph);
+    }
+    return this._bbox;
   },
   get strokes() {
-    return this.glyph.strokes;
+    return this._glyph.strokes;
+  },
+  get unitScaledStrokes() {
+    if (!this._unitScaledStrokes) {
+      this._unitScaledStrokes = FXGlyph.getScaledStrokes(this.strokes, this.bbox, 1);
+    }
+    return this._unitScaledStrokes;
   },
   get subStrokes() {
-    if (!this._features) {
-      this._features = new GlyphFeatures(this.glyph);
+    if (!this._subStrokes) {
+      this._subStrokes = this._getSubStrokes();
     }
-    return this._features.subStrokes;
+    return this._subStrokes;
   },
   get size() {
-    if (!this._features) {
-      this._features = new GlyphFeatures(this.glyph);
+    if (!this._sizes) {
+      this._calculateSizes();
     }
-    return this._features.size;
+    return this._size;
   },
+  get sizes() {
+    if (!this._sizes) {
+      this._calculateSizes();
+    }
+    return this._sizes;
+  },
+};
+
+// more methods
+
+FXGlyph.prototype._getBBox = function(glyph) {
+  var xy0 = glyph.strokes[0][0];
+  var bbox = {
+    top: xy0.y,
+    bottom: xy0.y,
+    left: xy0.x,
+    right: xy0.x,
+  };
+  glyph.strokes.forEach(function(stroke) {
+    stroke.forEach(function(xy) {
+      if (xy.x < bbox.left) {
+        bbox.left = xy.x;
+      } else if (xy.x > bbox.right) {
+        bbox.right = xy.x;
+      }
+      if (xy.y < bbox.top) {
+        bbox.top = xy.y;
+      } else if (xy.y > bbox.bottom) {
+        bbox.bottom = xy.y;
+      }
+    });
+  });
+  return bbox;
+};
+
+FXGlyph.prototype._calculateSizes = function() {
+  this._sizes = this.unitScaledStrokes.map(function(stroke) {
+    var s = 0;
+    var x = stroke[0].x;
+    var y = stroke[0].y;
+    for (var i = 1; i < stroke.length; i++) {
+      var nx = stroke[i].x;
+      var ny = stroke[i].y;
+      var dx = nx - x;
+      var dy = ny - y;
+      var ds = Math.sqrt(dx * dx + dy * dy);
+      s += ds;
+      x = nx;
+      y = ny;
+    }
+    return s;
+  });
+  this._size = this._sizes.reduce(function(tot, size) {
+    return tot + size;
+  }, 0);
+};
+
+FXGlyph.getScaledStrokes = function(strokes, bbox, scale) {
+  if (typeof scale === 'undefined') {
+    scale = 1;
+  }
+
+  var xScale = 1;
+  if (bbox.right > bbox.left) {
+    xScale = 1 / (bbox.right - bbox.left);
+  }
+  var yScale = 1;
+  if (bbox.bottom > bbox.top) {
+    yScale = 1 / (bbox.bottom - bbox.top);
+  }
+  var xyScale = Math.min(xScale, yScale);
+
+  var xMid = 0.5 * (bbox.left + bbox.right);
+  var yMid = 0.5 * (bbox.top + bbox.bottom);
+
+  function cx(x) {
+    return scale * (0.5 + (x - xMid) * xyScale);
+  }
+
+  function cy(y) {
+    return scale * (0.5 + (y - yMid) * xyScale);
+  }
+
+  var oStrokes = strokes.map(function(stroke) {
+    return stroke.map(function(xy) {
+      return {
+        x: cx(xy.x),
+        y: cy(xy.y)
+      };
+    });
+  });
+  return oStrokes;
+};
+
+FXGlyph.prototype._getSubStrokes = function() {
+  var allSubStrokes = [];
+  this.glyph.strokes.forEach(function(stroke) {
+    var subStrokes = getStrokeSubStrokes(stroke);
+    subStrokes.forEach(function(subStroke) {
+      allSubStrokes.push(subStroke);
+    });
+  });
+  allSubStrokes.forEach(function(subStroke) {
+    calculateSubstrokeSize(subStroke);
+  });
+  var subStrokeDescriptions = allSubStrokes.map(function(subStroke) {
+    return {
+      points: subStroke,
+      size: subStroke[subStroke.length - 1].s,
+    };
+  });
+  return subStrokeDescriptions;
 }
+
+function getStrokeSubStrokes(stroke) {
+  if (stroke.length <= 2) {
+    return [stroke];
+  }
+
+  var increments = [];
+  for (var i = 1; i < stroke.length; i++) {
+    var dx = stroke[i].x - stroke[i - 1].x;
+    var dy = stroke[i].y - stroke[i - 1].y;
+    increments.push({
+      dx: dx,
+      dy: dy
+    });
+  }
+
+  var breakPoints = [];
+  for (var i = 1; i < increments.length; i++) {
+    var i1 = increments[i - 1];
+    var i2 = increments[i];
+    var modi1 = Math.sqrt(i1.dx * i1.dx + i1.dy * i1.dy);
+    var modi2 = Math.sqrt(i2.dx * i2.dx + i2.dy * i2.dy);
+    var dot12 = i1.dx * i2.dx + i1.dy * i2.dy;
+    var cosTheta = dot12 / (modi1 * modi2);
+    if (cosTheta < -0.1) {
+      breakPoints.push(i);
+    }
+  }
+  breakPoints.push(stroke.length - 1);
+
+  var subStrokes = [];
+
+  function addSubStroke(startIndex, endIndex) {
+    var subStroke = [];
+    for (var i = startIndex; i <= endIndex; i++) {
+      subStroke.push({
+        x: stroke[i].x,
+        y: stroke[i].y
+      });
+    }
+    subStrokes.push(subStroke);
+  }
+
+  var startIndex = 0;
+  var breakPointIndex = 0;
+  while (breakPointIndex < breakPoints.length) {
+    var endIndex = breakPoints[breakPointIndex];
+    addSubStroke(startIndex, endIndex);
+    startIndex = endIndex;
+    breakPointIndex++;
+  }
+
+  return subStrokes;
+}
+
+function calculateSubstrokeSize(subStroke) {
+  var s = 0;
+  var i = 0;
+  for (i = 0; i < subStroke.length - 1; i++) {
+    var x0 = subStroke[i].x;
+    var y0 = subStroke[i].y;
+    var x1 = subStroke[i + 1].x;
+    var y1 = subStroke[i + 1].y;
+    var dx = x1 - x0;
+    var dy = y1 - y0;
+    var ds = Math.sqrt(dx * dx + dy * dy);
+    subStroke[i].s = s;
+    subStroke[i].ds = ds;
+    s += ds;
+  }
+  subStroke[i].s = s;
+}
+
+// export
 
 module.exports = FXGlyph;
 
-},{"./glyph":5,"./glyph-features":4}],3:[function(require,module,exports){
+},{"./glyph":4}],3:[function(require,module,exports){
 'use strict';
 
 var Glyph = require('./glyph');
@@ -74,26 +269,6 @@ GlyphBuilder.prototype.addPoint = function(xy) {
   }
 
   stroke.push(xy);
-
-  if (!self.bbox) {
-    self.bbox = {
-      top: xy.y,
-      bottom: xy.y,
-      left: xy.x,
-      right: xy.x
-    };
-  } else {
-    if (xy.x < self.bbox.left) {
-      self.bbox.left = xy.x;
-    } else if (xy.x > self.bbox.right) {
-      self.bbox.right = xy.x;
-    }
-    if (xy.y < self.bbox.top) {
-      self.bbox.top = xy.y;
-    } else if (xy.y > self.bbox.bottom) {
-      self.bbox.bottom = xy.y;
-    }
-  }
 };
 
 GlyphBuilder.prototype.endStroke = function() {
@@ -109,187 +284,43 @@ GlyphBuilder.prototype.endStroke = function() {
 };
 
 GlyphBuilder.prototype.getGlyph = function() {
-  var that = new Glyph();
-  that.strokes = this.strokes;
-  that.bbox = this.bbox;
-  return that;
+  return new Glyph({
+    strokes: this.strokes
+  });
 };
 
 module.exports = GlyphBuilder;
 
-},{"./glyph":5}],4:[function(require,module,exports){
+},{"./glyph":4}],4:[function(require,module,exports){
 'use strict';
 
-var Glyph = require('./glyph');
+var Glyph = function(spec) {
+  this.id = (spec && spec.id) || "";
+  this.device = (spec && spec.device) || "";
+  this.strokes = (spec && spec.strokes && normalizeStrokes(spec.strokes)) || [];
+};
 
-var GlyphFeatures = function(glyph) {
-
-  // public API and instance variables
-  var self = this;
-  self.glyph = glyph;
-
-  self.bbox = getBoundingBox(self.glyph);
-  self.subStrokes = getGlyphSubStrokes(self.glyph);
-  self.size = getSubStrokeSizes(self.subStrokes);
-
-  function getBoundingBox(glyph) {
-    return glyph.bbox;
-  }
-
-  function getGlyphSubStrokes(glyph) {
-    var res = [];
-    glyph.strokes.forEach(function(stroke) {
-      var subStrokes = getStrokeSubStrokes(stroke);
-      subStrokes.forEach(function(subStroke) {
-        res.push(subStroke);
-      });
-    });
-    return res;
-  }
-
-  function getStrokeSubStrokes(stroke) {
-    if (stroke.length <= 2) {
-      return [{
-        points: stroke
-      }];
+function normalizeStrokes(strokesSpec) {
+  return strokesSpec.map(function(strokeSpec) {
+    if (typeof strokeSpec !== 'object') {
+      throw 'bad stroke spec: must be object';
     }
-
-    var increments = [];
-    for (var i = 1; i < stroke.length; i++) {
-      var dx = stroke[i].x - stroke[i - 1].x;
-      var dy = stroke[i].y - stroke[i - 1].y;
-      increments.push({
-        dx: dx,
-        dy: dy
-      });
-    }
-
-    var breakPoints = [];
-    for (var i = 1; i < increments.length; i++) {
-      var i1 = increments[i - 1];
-      var i2 = increments[i];
-      var modi1 = Math.sqrt(i1.dx * i1.dx + i1.dy * i1.dy);
-      var modi2 = Math.sqrt(i2.dx * i2.dx + i2.dy * i2.dy);
-      var dot12 = i1.dx * i2.dx + i1.dy * i2.dy;
-      var cosTheta = dot12 / (modi1 * modi2);
-      if (cosTheta < -0.1) {
-        breakPoints.push(i);
-      }
-    }
-    breakPoints.push(stroke.length - 1);
-
-    var subStrokes = [];
-
-    function addSubStroke(startIndex, endIndex) {
-      var subStroke = [];
-      for (var i = startIndex; i <= endIndex; i++) {
-        subStroke.push({
-          x: stroke[i].x,
-          y: stroke[i].y
+    if (typeof strokeSpec[0] === 'number') {
+      var stroke = [];
+      for (var i = 0; i < strokeSpec.length; i += 2) {
+        stroke.push({
+          x: strokeSpec[i],
+          y: strokeSpec[i + 1]
         });
       }
-      subStrokes.push({
-        points: subStroke
-      });
+      return stroke;
+    } else if (typeof strokeSpec[0] === 'object') {
+      return strokeSpec;
+    } else {
+      throw 'bad stroke spec: first must be number or (x,y) pair';
     }
-
-    var startIndex = 0;
-    var breakPointIndex = 0;
-    while (breakPointIndex < breakPoints.length) {
-      var endIndex = breakPoints[breakPointIndex];
-      addSubStroke(startIndex, endIndex);
-      startIndex = endIndex;
-      breakPointIndex++;
-    }
-
-    return subStrokes;
-  }
-
-  function getSubStrokeSizes(subStrokes) {
-    var size = subStrokes.reduce(function(previousSize, subStroke) {
-      return subStrokeSize(subStroke) + previousSize;
-    }, 0);
-    return size;
-  }
-
-  function subStrokeSize(subStroke) {
-    var s = 0;
-    var i = 0;
-    for (i = 0; i < subStroke.points.length - 1; i++) {
-      var x0 = subStroke.points[i].x;
-      var y0 = subStroke.points[i].y;
-      var x1 = subStroke.points[i + 1].x;
-      var y1 = subStroke.points[i + 1].y;
-      var dx = x1 - x0;
-      var dy = y1 - y0;
-      var ds = Math.sqrt(dx * dx + dy * dy);
-      subStroke.points[i].s = s;
-      subStroke.points[i].ds = ds;
-      s += ds;
-    }
-    subStroke.points[i].s = s;
-    subStroke.size = s;
-    return subStroke.size;
-  }
-};
-
-module.exports = GlyphFeatures;
-
-},{"./glyph":5}],5:[function(require,module,exports){
-'use strict';
-
-var Glyph = function() {
-
-  // public API and instance variables
-  var self = this;
-  self.strokes = [];
-  self.bbox = undefined;
-};
-
-// implementation -- methods
-Glyph.prototype.getScaled = function(scale) {
-  var self = this;
-
-  var xScale = 0;
-  if (self.bbox.right > self.bbox.left) {
-    xScale = scale / (self.bbox.right - self.bbox.left);
-  }
-  var x0 = -self.bbox.left * scale;
-  var yScale = 0;
-  if (self.bbox.bottom > self.bbox.top) {
-    yScale = scale / (self.bbox.bottom - self.bbox.top);
-  }
-  var y0 = -self.bbox.top * scale;
-
-  function cx(x) {
-    return x * xScale + x0;
-  }
-
-  function cy(y) {
-    return y * yScale + y0;
-  }
-
-  var that = new Glyph();
-  that.strokes = self.strokes.map(function(stroke) {
-    return stroke.map(function(xy) {
-      return {
-        x: cx(xy.x),
-        y: cy(xy.y)
-      };
-    });
   });
-  that.bbox = {
-    left: cx(self.bbox.left),
-    right: cx(self.bbox.right),
-    top: cy(self.bbox.top),
-    bottom: cy(self.bbox.bottom),
-  };
-  return that;
-};
-
-Glyph.prototype.getUnitScaled = function() {
-  return this.getScaled(1.0);
-};
+}
 
 module.exports = Glyph;
 
