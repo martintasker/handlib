0e656cee28bb7678bd9e545a201f1dace0ad84a8
Martin Tasker <martin.tasker@databatix.com>
2016-10-24 12:14:22 +0100

    box-transformer: added code and test code

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ module.exports.Glyph = require('./lib/glyph');
 module.exports.GlyphBuilder = require('./lib/glyph-builder');
 module.exports.FXGlyph = require('./lib/fx-glyph');
 module.exports.BoundingBox = require('./lib/bounding-box');
+module.exports.BoxTransformer = require('./lib/box-transformer');
